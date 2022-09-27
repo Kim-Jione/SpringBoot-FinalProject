4acd0019fc38e3525e6a3fc0d8ad3d4db14255cf
Kimjiwon <hghg1036@naver.com>
2022-09-27 11:03:32 +0900

    경기장 등록기능 완료

--- src/main/java/site/metacoding/finalproject/service/StadiumService.java
+++ src/main/java/site/metacoding/finalproject/service/StadiumService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.finalproject.domain.stadium.Stadium;
 import site.metacoding.finalproject.domain.stadium.StadiumDao;
+import site.metacoding.finalproject.web.dto.stadium.StadiumInsertReqDto;
 
 // 서비스는 Dao를 조합하고 트랜잭션을 관리한다
 @RequiredArgsConstructor
@@ -18,5 +19,9 @@ public class StadiumService {
 
 	public List<Stadium> 목록보기(){
 		return stadiumDao.findAll();
+	}
+	// Controller는 DTO로 받고 - 유효성 검사, DAO는 Entity로 넘기자 (Insert, Update) - 유효성 검사 X
+	public void 경기장등록(StadiumInsertReqDto stadiumInsertReqDto) { //Dto로 받는 이유는 엔티티로 받을 때 유효성 체크가 힘들어서 Dto로 받는 것이다. Dao한테 넘길 때는 엔티티를 넘겨 주면 된다. 앞에서 밸리데이션 체크 받을것이기 때문
+		stadiumDao.insert(stadiumInsertReqDto.toEntity());
 	}
 }
--- src/main/java/site/metacoding/finalproject/web/StadiumController.java
+++ src/main/java/site/metacoding/finalproject/web/StadiumController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.finalproject.domain.stadium.Stadium;
 import site.metacoding.finalproject.service.StadiumService;
+import site.metacoding.finalproject.web.dto.CMRespDto;
+import site.metacoding.finalproject.web.dto.stadium.StadiumInsertReqDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +26,16 @@ public class StadiumController {
 		List<Stadium> stadiumList = stadiumService.목록보기();
 		model.addAttribute("stadiumList", stadiumList);
 		return "stadium/list";
+	}
+	
+	@GetMapping("/stadiumForm")
+	public String stadiumForm() {
+		return "stadium/saveForm";
+	}
+	
+	@PostMapping("/stadium")
+	public @ResponseBody CMRespDto<?> insert(@RequestBody StadiumInsertReqDto stadiumInsertReqDto){
+		stadiumService.경기장등록(stadiumInsertReqDto);
+		return new CMRespDto<>(1, "경기장등록성공", null);
 	}
 }
